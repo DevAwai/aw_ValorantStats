9807fb9409a8d242d19e067bc8e2ef7639cf573a
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-03-31 02:54:17 +0200

    only mwa qui course

--- Commandes/course.js
+++ Commandes/course.js
@@ -4,8 +4,17 @@ const { animateRace } = require("../utils/raceManager");
 module.exports = {
     name: "course",
     cooldown: 2000,
-    description: "GAGAY",
+    description: "Démarre une course de chevaux",
     async execute(interaction) {
+        const allowedUserId = "663844641250213919";
+
+        if (interaction.user.id !== allowedUserId) {
+            return interaction.reply({
+                content: "❌ Vous n'êtes pas autorisé à utiliser cette commande.",
+                ephemeral: true,
+            });
+        }
+
         await interaction.reply("🏇 La course va bientôt commencer !");
         const winner = await animateRace(interaction.channel);
         await interaction.followUp(`🎉 Félicitations au cheval **${winner}** pour sa victoire !`);
